93d961a9eac01f1a56d94830f39a652a1504ec40
neox56 <neox56@gmail.com>
2019-01-28 13:48:00 +0400

    add weekForecast

--- js/weather-details.js
+++ js/weather-details.js
@@ -23,22 +23,26 @@ function showButtons(k) {
 
 const APPID = "e33262cd6a432b1c3dc5181a736dbc41";
 const WEATHER_DETAILS_ENDPOINT = `http://api.openweathermap.org/data/2.5/weather?units=metric&APPID=${APPID}&lang=ru&q=`;
-const AIR_POLLUTIONS_DETAILS = `http://api.openweathermap.org/pollution/v1/co/53,21/current.json?appid=${APPID}`
+const AIR_POLLUTIONS_DETAILS = `http://api.openweathermap.org/pollution/v1/co/53,21/current.json?appid=${APPID}`;
+const WEATHER_DETAILS_FIVE_DAYS = `http://api.openweathermap.org/data/2.5/forecast?&units=metric&appid=${APPID}&lang=ru&q=`;
 const defaultCity = "Izhevsk";
+
 
 const page = {
   init: function () {
-    this.getWeatherDetails(defaultCity, this.render,WEATHER_DETAILS_ENDPOINT);
-    this.getWeatherDetails(defaultCity, this.renderPollution,AIR_POLLUTIONS_DETAILS);
+    this.getWeatherDetails(defaultCity, this.render, WEATHER_DETAILS_ENDPOINT);
+    this.getPollution(defaultCity, this.renderPollution, AIR_POLLUTIONS_DETAILS);
+    this.getWeatherDetails(defaultCity, this.renderfiveDays, WEATHER_DETAILS_FIVE_DAYS);
     const searchField = document.getElementById("search-field");
     searchField.addEventListener("change", (event) => {
       const city = event.target.value;
-      this.getWeatherDetails(city, this.render,WEATHER_DETAILS_ENDPOINT);
-      this.getWeatherDetails(city, this.renderPollution,AIR_POLLUTIONS_DETAILS)
+      this.getWeatherDetails(city, this.render, WEATHER_DETAILS_ENDPOINT);
+      this.getWeatherDetails(city, this.renderfiveDays, WEATHER_DETAILS_FIVE_DAYS);
+      this.getPollution(defaultCity, this.renderPollution, AIR_POLLUTIONS_DETAILS);
     });
   },
 
-  getWeatherDetails(city, callback, ) {
+  getWeatherDetails(city, callback, WEATHER_DETAILS_ENDPOINT) {
     const url = `${WEATHER_DETAILS_ENDPOINT}${city}`;
     const xhr = new XMLHttpRequest();
 
@@ -52,10 +56,20 @@ const page = {
     xhr.open("GET", url, true); //настройка запроса
     xhr.send(); // инициализация соединения; метод открывает соединение и отправляет запрос на сервер.
   },
-
+  getPollution(city, callback, AIR_POLLUTIONS_DETAILS) {
+    const url = `${AIR_POLLUTIONS_DETAILS}`;
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        console.log(JSON.parse(xhr.responseText));
+        callback(JSON.parse(xhr.responseText));
+      }
+    };
+    xhr.open('GET', url, true);
+    xhr.send();
+  },
 
   render(data) {
-
     const date = new Date();
     const city = data.name;
     const country = data.sys.country;
@@ -76,11 +90,57 @@ const page = {
     document.getElementById('precipitation').innerHTML = `Вероятность осадков: ${precipitation}%`;
 
   },
-
   renderPollution(data) {
-    const airPollution = Math.random(data.value)*10+'e-8';
+    const airPollution = Math.random(data.value) * 10 + 'e-8';
     document.getElementById('air-pollution').innerHTML = `Загрязнение воздуха: ${airPollution}`;
-  }
-}
+  },
 
+  renderfiveDays(data) {
+    let dayWeek = document.querySelector(".container-days"),
+      number = "",
+      weekForecast = data.list.filter(item => item.dt_txt.indexOf('18:00:00') > -1);
+    for (let i = 0; i < weekForecast.length; i++) {
+      let date = weekForecast[i].dt_txt;
+      icon = weekForecast[i].weather[0].icon;
+      typeWeather = weekForecast[i].weather[0].description;
+      maxTemp = weekForecast[i].main.temp_max;
+      minTemp = weekForecast[i].main.temp_min;
+      number += `<section class="container-days">
+                        <span>${getDayName(date)}</span>
+                        <figure>
+                            <img src="http://openweathermap.org/img/w/${icon}.png">
+                        </figure>
+                        <figcaption>
+                            <span id="maxTemp">${maxTemp.toFixed()}°</span>
+                            <span id="minTemp">${minTemp.toFixed()}°</span>
+                        </figcaption>
+                    </section>`
+    }
+    dayWeek.innerHTML = number;
+  }
+};
 page.init();
+
+
+function getDayName(date) {
+  let dayName = "";
+  date = new Date(date);
+  switch (date.getDay()) {
+    case 1:
+      dayName = "Пн";
+      break;
+    case 2:
+      dayName = "Вт";
+      break;
+    case 3:
+      dayName = "Ср";
+      break;
+    case 4:
+      dayName = "Чт";
+      break;
+    case 5:
+      dayName = "Пт";
+      break;
+  }
+  return dayName;
+}
